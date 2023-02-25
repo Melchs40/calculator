788df3cc98068d43f75f40cb9254283ba21ae74c
Melchs40 <samscodingjourney@gmail.com>
2023-02-25 01:02:08 -0600

    Create division function

--- test.js
+++ test.js
@@ -21,3 +21,11 @@ function mulitplication (...numbers) {
     };
     return total;
 };
+
+function division (...numbers) {
+    let total = numbers[0];
+    for(let i = 1; i < numbers.length; i++) {
+        total /= numbers[i];
+    };
+    return total;
+};
